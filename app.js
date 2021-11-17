e98b3daf09074589263cbc8345c6925df63d3ca9
isanfelix <farisihsan95@gmail.com>
2021-11-17 08:33:08 +0700

    #5 --add require express, cors, json, urlencoded

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes/index')
+const app = express()
+const port = process.env.port || 3000
+
+const { User } = require('./models/index')
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', routes)
+
+app.listen(port, () => {
+  console.log(`Listening port : ${port} with url http://127.0.0.1:${port}`);
+}) 
